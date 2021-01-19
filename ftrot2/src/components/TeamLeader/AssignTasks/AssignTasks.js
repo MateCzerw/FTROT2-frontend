8f57e4423a41a0bf46e408f3d1fdc35f955093a5
Czerwo <mateczerw@gmail.com>
2021-01-19 23:12:02 +0100

    Css Update

--- ftrot2/src/components/TeamLeader/AssignTasks/AssignTasks.js
+++ ftrot2/src/components/TeamLeader/AssignTasks/AssignTasks.js
@@ -8,16 +8,14 @@ const itemsFromBackend = [
   { id: uuid(), content: "Third task" },
   { id: uuid(), content: "Fourth task" },
   { id: uuid(), content: "Fifth task" },
-  { id: uuid(), content: "6 task" },
-  { id: uuid(), content: "7 task" },
-  { id: uuid(), content: "8 task" },
-  { id: uuid(), content: "9 task" },
-  { id: uuid(), content: "10 task" },
-  { id: uuid(), content: "11 task" },
-  { id: uuid(), content: "12 task" },
-  { id: uuid(), content: "13 task" },
-  { id: uuid(), content: "14 task" },
-  { id: uuid(), content: "15 task" },
+];
+
+const anotherItems = [
+  { id: uuid(), content: "First task" },
+  { id: uuid(), content: "Second task" },
+  { id: uuid(), content: "Third task" },
+  { id: uuid(), content: "Fourth task" },
+  { id: uuid(), content: "Fifth task" },
 ];
 
 const findEngineerByColumnId = () => {};
@@ -27,7 +25,7 @@ const columnsFTROT = [
     engineer: "Mateusz Czerwiński",
     week: 1,
     schedule: [
-      { columnId: uuid(), dayName: "Monday", tasks: [] },
+      { columnId: uuid(), dayName: "Monday", tasks: anotherItems },
       { columnId: uuid(), dayName: "Tuesday", tasks: [] },
       { columnId: uuid(), dayName: "Wednesday", tasks: [] },
       { columnId: uuid(), dayName: "Thursday", tasks: [] },
@@ -37,7 +35,7 @@ const columnsFTROT = [
   {
     engineer: "Bartosz Kozłowski",
     week: 1,
-    tasksInWeek: [
+    schedule: [
       { columnId: uuid(), day: "Monday", tasks: [] },
       { columnId: uuid(), day: "Tuesday", tasks: [] },
       { columnId: uuid(), day: "Wednesday", tasks: [] },
@@ -48,7 +46,7 @@ const columnsFTROT = [
   {
     engineer: "Agnieszka Leszczuk",
     week: 1,
-    tasksInWeek: [
+    schedule: [
       { columnId: uuid(), day: "Monday", tasks: [] },
       { columnId: uuid(), day: "Tuesday", tasks: [] },
       { columnId: uuid(), day: "Wednesday", tasks: [] },
@@ -59,7 +57,7 @@ const columnsFTROT = [
   {
     engineer: "Marek Repeła",
     week: 1,
-    tasksInWeek: [
+    schedule: [
       { columnId: uuid(), day: "Monday", tasks: [] },
       { columnId: uuid(), day: "Tuesday", tasks: [] },
       { columnId: uuid(), day: "Wednesday", tasks: [] },
@@ -76,31 +74,56 @@ const unassignedTasksFromBackend = {
   },
 };
 
-const columnsFromBackend = {
-  [uuid()]: {
-    name: "Monday",
-    items: [],
-  },
-  [uuid()]: {
-    name: "Tuesday",
-    items: [],
-  },
-  [uuid()]: {
-    name: "Wednesday",
-    items: [],
-  },
-  [uuid()]: {
-    name: "Thursday",
-    items: [],
-  },
-  [uuid()]: {
-    name: "Friday",
-    items: [],
-  },
+const generateRawForOneWeek = () => {
+  return <div></div>;
+};
+
+const generateColumnForOneDay = (columnId, tasks) => {
+  return (
+    <Droppable droppableId={columnId} key={columnId}>
+      {(provided, snapshot) => {
+        return (
+          <div
+            {...provided.droppableProps}
+            ref={provided.innerRef}
+            className="tasks__dayContainer"
+            style={{
+              background: snapshot.isDraggingOver ? "lightblue" : "lightgrey",
+            }}
+          >
+            {tasks.map((task, index) => {
+              return (
+                <Draggable key={task.id} draggableId={task.id} index={index}>
+                  {(provided, snapshot) => {
+                    return (
+                      <div
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                        className="tasks__eachTask"
+                        style={{
+                          backgroundColor: snapshot.isDragging
+                            ? "#263B4A"
+                            : "tomato",
+                          ...provided.draggableProps.style,
+                        }}
+                      >
+                        {task.content}
+                      </div>
+                    );
+                  }}
+                </Draggable>
+              );
+            })}
+            {provided.placeholder}
+          </div>
+        );
+      }}
+    </Droppable>
+  );
 };
 
 const onDragEnd = (result, columns, setColumns) => {
-  console.log(columnsFromBackend);
   // console.log(columns);
   // console.log(setColumns);
   if (!result.destination) return;
@@ -141,154 +164,83 @@ const onDragEnd = (result, columns, setColumns) => {
 
 const AssignTasks = () => {
   // const [columns, setColumns] = useState(columnsFromBackend);
-  const [columns, setColumns] = useState(columnsFromBackend);
+  const [columns, setColumns] = useState(columnsFTROT);
   const [unassignedTasks, setUnassignedTasks] = useState(
     unassignedTasksFromBackend
   );
 
   return (
-    <div style={{ display: "flex", justifyContent: "center", height: "100%" }}>
-      <DragDropContext
-        onDragEnd={(result) => onDragEnd(result, columns, setColumns)}
-      >
-        <div className="tasks__container">
-          <div className="tasks__unassigned">
-            {Object.entries(unassignedTasks).map(
-              ([columnId, column], index) => {
-                return (
-                  <div key={columnId}>
-                    <h2>{column.name}</h2>
-                    <div style={{ margin: 8 }}>
-                      <Droppable droppableId={columnId} key={columnId}>
-                        {(provided, snapshot) => {
-                          return (
-                            <div
-                              className="tasks__day"
-                              {...provided.droppableProps}
-                              ref={provided.innerRef}
-                              style={{
-                                background: snapshot.isDraggingOver
-                                  ? "lightblue"
-                                  : "lightgrey",
-                              }}
-                            >
-                              {column.items.map((item, index) => {
-                                return (
-                                  <Draggable
-                                    key={item.id}
-                                    draggableId={item.id}
-                                    index={index}
-                                  >
-                                    {(provided, snapshot) => {
-                                      return (
-                                        <div
-                                          ref={provided.innerRef}
-                                          {...provided.draggableProps}
-                                          {...provided.dragHandleProps}
-                                          style={{
-                                            userSelect: "none",
-                                            padding: 16,
-                                            margin: "0 0 8px 0",
-                                            minHeight: "50px",
-                                            backgroundColor: snapshot.isDragging
-                                              ? "#263B4A"
-                                              : "#456C86",
-                                            color: "white",
-                                            ...provided.draggableProps.style,
-                                          }}
-                                        >
-                                          {item.content}
-                                        </div>
-                                      );
-                                    }}
-                                  </Draggable>
-                                );
-                              })}
-                              {provided.placeholder}
-                            </div>
-                          );
+    <DragDropContext
+      onDragEnd={(result) => onDragEnd(result, columns, setColumns)}
+    >
+      <div className="tasks__container">
+        <div className="tasks__unassignedTasksBoard">
+          {Object.entries(unassignedTasks).map(([columnId, column], index) => {
+            return (
+              <div key={columnId}>
+                <h2>Unassigned tasks</h2>
+
+                <Droppable droppableId={columnId} key={columnId}>
+                  {(provided, snapshot) => {
+                    return (
+                      <div
+                        className="tasks__eachDay"
+                        {...provided.droppableProps}
+                        ref={provided.innerRef}
+                        style={{
+                          background: snapshot.isDraggingOver
+                            ? "lightblue"
+                            : "lightgrey",
                         }}
-                      </Droppable>
-                    </div>
-                  </div>
-                );
-              }
-            )}
-          </div>
-          <div className="tasks__engineerContainer">
-            {columnsFTROT[0].schedule.map((day, index) => {
-              const { columnId, dayName, tasks } = day;
-              return (
-                <div
-                  style={{
-                    display: "flex",
-                    flexDirection: "column",
-                    alignItems: "center",
+                      >
+                        {column.items.map((item, index) => {
+                          return (
+                            <Draggable
+                              key={item.id}
+                              draggableId={item.id}
+                              index={index}
+                            >
+                              {(provided, snapshot) => {
+                                return (
+                                  <div
+                                    ref={provided.innerRef}
+                                    {...provided.draggableProps}
+                                    {...provided.dragHandleProps}
+                                    className="tasks__eachTask"
+                                    style={{
+                                      backgroundColor: snapshot.isDragging
+                                        ? "#263B4A"
+                                        : "#456C86",
+                                      ...provided.draggableProps.style,
+                                    }}
+                                  >
+                                    {item.content}
+                                  </div>
+                                );
+                              }}
+                            </Draggable>
+                          );
+                        })}
+                        {provided.placeholder}
+                      </div>
+                    );
                   }}
-                  key={columnId}
-                >
-                  <h2>{dayName}</h2>
-                  <div style={{ margin: 8 }}>
-                    <Droppable droppableId={columnId} key={columnId}>
-                      {(provided, snapshot) => {
-                        return (
-                          <div
-                            {...provided.droppableProps}
-                            ref={provided.innerRef}
-                            style={{
-                              background: snapshot.isDraggingOver
-                                ? "lightblue"
-                                : "lightgrey",
-                              padding: 4,
-                              width: 250,
-                              minHeight: 500,
-                            }}
-                          >
-                            {tasks.map((task, index) => {
-                              return (
-                                <Draggable
-                                  key={task.id}
-                                  draggableId={task.id}
-                                  index={index}
-                                >
-                                  {(provided, snapshot) => {
-                                    return (
-                                      <div
-                                        ref={provided.innerRef}
-                                        {...provided.draggableProps}
-                                        {...provided.dragHandleProps}
-                                        style={{
-                                          userSelect: "none",
-                                          padding: 16,
-                                          margin: "0 0 8px 0",
-                                          minHeight: "50px",
-                                          backgroundColor: snapshot.isDragging
-                                            ? "#263B4A"
-                                            : "#456C86",
-                                          color: "white",
-                                          ...provided.draggableProps.style,
-                                        }}
-                                      >
-                                        {task.content}
-                                      </div>
-                                    );
-                                  }}
-                                </Draggable>
-                              );
-                            })}
-                            {provided.placeholder}
-                          </div>
-                        );
-                      }}
-                    </Droppable>
-                  </div>
-                </div>
-              );
-            })}
-          </div>
+                </Droppable>
+              </div>
+            );
+          })}
         </div>
-      </DragDropContext>
-    </div>
+
+        <div className="tasks__engineersBoard">
+          {columnsFTROT.map((column, index) => {
+            return column.schedule.map((day) => {
+              const { columnId, dayName, tasks } = day;
+              return generateColumnForOneDay(columnId, tasks);
+            });
+          })}
+        </div>
+      </div>
+    </DragDropContext>
   );
 };
 
