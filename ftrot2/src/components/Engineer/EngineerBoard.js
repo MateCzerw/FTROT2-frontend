f7a0a43659fe0a21a6efe8964cc0b6eea9a84496
Czerwo <mateczerw@gmail.com>
2021-01-26 22:40:16 +0100

    EngineerBoard modification

--- ftrot2/src/components/Engineer/EngineerBoard.js
+++ ftrot2/src/components/Engineer/EngineerBoard.js
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import "./EngineerBoard.css";
 import Slider from "@material-ui/core/Slider";
-import { Pie } from "react-chartjs-2";
+import { Doughnut, Pie } from "react-chartjs-2";
+
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import moment from "moment";
 
 const contentInfo = {
   name: "Mateusz",
@@ -9,72 +13,83 @@ const contentInfo = {
   team: "DLSC2",
   role: "Lead Eangineer",
   supervisor: "Wojciech Zabiegło",
-  joinedAt: Date.now(),
+  joinedAt: moment(Date.now()).calendar(),
   picture: "",
   currentTasks: [
-    { taskName: "Wypełnić Ftrot", status: 0.9, estimatedTime: 4 },
-    { taskName: "Zrobić rysunek", status: 1, estimatedTime: 4 },
-    { taskName: "Zrobić model obudowy", status: 0.9, estimatedTime: 4 },
-    { taskName: "Zrobić model Pedału", status: 1, estimatedTime: 4 },
-    { taskName: "Zrobić wniosek Patentowy", status: 0.9, estimatedTime: 4 },
-    { taskName: "Zrobić efficieny", status: 1, estimatedTime: 4 },
+    { name: "Wypełnić Ftrot", status: 0.9, estimatedTime: 4 },
+    { name: "Zrobić rysunek", status: 1, estimatedTime: 4 },
+    { name: "Zrobić model obudowy", status: 0.9, estimatedTime: 4 },
+    { name: "Zrobić model Pedału", status: 1, estimatedTime: 4 },
+    { name: "Zrobić wniosek Patentowy", status: 0.9, estimatedTime: 4 },
+    { name: "Zrobić efficieny", status: 1, estimatedTime: 4 },
   ],
 };
 
 const EngineerBoard = () => {
   return (
-    <main className="content">
-      <section className="content__panel">
-        <h2 className="content__title">User Info</h2>
-        <article className="content__userDetails">
+    <main className="engineer board__background">
+      <section className="engineer board__container">
+        <article className="engineer board__userDetails">
           <img
             src="https://yt3.ggpht.com/yti/ANoDKi6wK_UXTj-paYQq980Ia30B623dBP5hTFc9Fnsciw=s88-c-k-c0x00ffffff-no-rj-mo"
             alt="Mateusz Czerwiński"
           ></img>
-          <div>
-            <h3>Name: {contentInfo.name}</h3>
-            <h3>Surname: {contentInfo.surname}</h3>
+          <div className="engineer board__statsColumn">
+            <p className="engineer board__stat">
+              <b>Name:</b> {contentInfo.name}
+            </p>
+            <p className="engineer board__stat">
+              <b>Surname:</b> {contentInfo.surname}
+            </p>
           </div>
-          <div>
-            <h3>Team: {contentInfo.team}</h3>
-            <h3>Role: {contentInfo.role}</h3>
+          <div className="engineer board__statsColumn">
+            <p className="engineer board__stat">
+              <b>Team:</b> {contentInfo.team}
+            </p>
+            <p className="engineer board__stat">
+              <b>Role:</b> {contentInfo.role}
+            </p>
+          </div>
+          <div className="engineer board__statsColumn">
+            <p className="engineer board__stat">
+              <b>Supervisor:</b> {contentInfo.supervisor}
+            </p>
+            <p className="engineer board__stat">
+              <b>Joined at:</b> {contentInfo.joinedAt}
+            </p>
           </div>
         </article>
-        <article className="content__tasks">
-          <ol className="content__tasklist">
-            {/* {console.log( */}
-            {contentInfo.currentTasks.map((task, i) => (
-              <li key={i}>
-                <p>{task.taskName}</p>
-                <Slider
-                  defaultValue={task.status * 100}
-                  aria-labelledby="discrete-slider"
-                  valueLabelDisplay="auto"
-                  step={10}
-                  marks
-                  min={0}
-                  max={100}
-                />
+        <article className="engineer board__panel">
+          <ul className="engineer board__tasks">
+            {contentInfo.currentTasks.map((task) => (
+              <li className="engineer board__task">
+                <div
+                  className="engineer board__taskStatus"
+                  style={{ width: "50%" }}
+                ></div>
+                <p className="engineer board__taskDescription">
+                  {`Name:
+              ${task.name} Status: ${task.status * 100}% Estimated time: 
+              ${task.estimatedTime}`}
+                </p>
               </li>
             ))}
-          </ol>
-          <div className="content__statistics">
-            <Pie
+          </ul>
+          <div className="engineer board__graph">
+            <Doughnut
               data={{
-                labels: contentInfo.currentTasks.map((task) => task.taskName),
+                labels: ["Done", "In progress", "Deleyed"],
                 datasets: [
                   {
-                    data: contentInfo.currentTasks.map(
-                      (task) => task.status * task.estimatedTime
-                    ),
+                    data: [90, 32, 30],
                     backgroundColor: ["green", "orange", "gray"],
                   },
                 ],
               }}
-              width={"50%"}
-              height={"50%"}
-              options={{ maintainAspectRatio: false }}
-            ></Pie>
+              width={"100%"}
+              height={"100%"}
+              options={{ maintainAspectRatio: false, fontColor: "black" }}
+            ></Doughnut>
           </div>
         </article>
       </section>
