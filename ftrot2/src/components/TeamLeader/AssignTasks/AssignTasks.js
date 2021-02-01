560b1fef24fe1284856c2acc27b80545b1e0552d
Czerwo <mateczerw@gmail.com>
2021-02-01 20:57:14 +0100

    unassigned tasks logic moved another component

--- ftrot2/src/components/TeamLeader/AssignTasks/AssignTasks.js
+++ ftrot2/src/components/TeamLeader/AssignTasks/AssignTasks.js
@@ -118,68 +118,6 @@ const unassignedTasksFromBackend = {
   tasks: itemsFromBackend,
 };
 
-// const generateColumnForOneDay = (columnId, tasks, dayName) => {
-//   return (
-//     <Droppable droppableId={columnId} key={columnId}>
-//       {(provided, snapshot) => {
-//         return (
-//           <div
-//             {...provided.droppableProps}
-//             ref={provided.innerRef}
-//             className="teamLeader assignTasks tasks__dayContainer"
-//             style={{
-//               background: snapshot.isDraggingOver ? "lightblue" : "lightgrey",
-//             }}
-//           >
-//             <h3>{`${dayName} | ${calculateTotalHoursInColumn(tasks)}h`}</h3>
-
-//             {tasks.map((task, index) => {
-//               return (
-//                 <Draggable key={task.id} draggableId={task.id} index={index}>
-//                   {(provided, snapshot) => {
-//                     return (
-//                       <div
-//                         ref={provided.innerRef}
-//                         {...provided.draggableProps}
-//                         {...provided.dragHandleProps}
-//                         className="teamLeader assignTasks tasks__eachTask"
-//                         style={{
-//                           backgroundColor: snapshot.isDragging
-//                             ? "#263B4A"
-//                             : "tomato",
-//                           ...provided.draggableProps.style,
-//                         }}
-//                       >
-//                         <p>{task.content}</p>
-//                         <p>{task.duration}</p>
-//                       </div>
-//                     );
-//                   }}
-//                 </Draggable>
-//               );
-//             })}
-//             {provided.placeholder}
-//           </div>
-//         );
-//       }}
-//     </Droppable>
-//   );
-// };
-
-const calculateTotalHoursInColumn = (tasks) => {
-  let counter = 0;
-  tasks.forEach((task) => (counter += task.duration));
-  return counter;
-};
-
-const calculateTotalHoursInRow = (schedule) => {
-  let counter = 0;
-  schedule.forEach((day) => {
-    day.tasks.forEach((task) => (counter += task.duration));
-  });
-  return counter;
-};
-
 const onDragEnd = (
   result,
   columns,
@@ -336,62 +274,11 @@ const AssignTasks = () => {
     >
       <div className="teamLeader assignTasks tasks__container">
         <div className="teamLeader assignTasks tasks__unassignedTasksBoard">
-          <div key={unassignedTasks.columnId}>
-            <h2>{`${unassignedTasks.name} | ${calculateTotalHoursInColumn(
-              unassignedTasks.tasks
-            )}h`}</h2>
-
-            <Droppable
-              droppableId={unassignedTasks.columnId}
-              key={unassignedTasks.columnId}
-            >
-              {(provided, snapshot) => {
-                return (
-                  <div
-                    className="teamLeader assignTasks tasks__unasignedTasksColumn"
-                    {...provided.droppableProps}
-                    ref={provided.innerRef}
-                    style={{
-                      background: snapshot.isDraggingOver
-                        ? "lightblue"
-                        : "lightgrey",
-                    }}
-                  >
-                    {unassignedTasks.tasks.map((task, index) => {
-                      return (
-                        <Draggable
-                          key={task.id}
-                          draggableId={task.id}
-                          index={index}
-                        >
-                          {(provided, snapshot) => {
-                            return (
-                              <div
-                                ref={provided.innerRef}
-                                {...provided.draggableProps}
-                                {...provided.dragHandleProps}
-                                className="tasks__eachTask"
-                                style={{
-                                  backgroundColor: snapshot.isDragging
-                                    ? "#263B4A"
-                                    : "tomato",
-                                  ...provided.draggableProps.style,
-                                }}
-                              >
-                                <p>{task.content}</p>
-                                <p>{task.duration}</p>
-                              </div>
-                            );
-                          }}
-                        </Draggable>
-                      );
-                    })}
-                    {provided.placeholder}
-                  </div>
-                );
-              }}
-            </Droppable>
-          </div>
+          <TasksColumn
+            columnId={unassignedTasks.columnId}
+            tasks={unassignedTasks.tasks}
+            dayName={unassignedTasks.name}
+          />
         </div>
 
         <div className="teamLeader assignTasks  tasks__engineersBoard">
