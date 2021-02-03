d5a88f10f145a19968b40172e92bfb3543d7564c
Czerwo <mateczerw@gmail.com>
2021-02-03 23:28:39 +0100

    added redux action for LeadEngineer workpackages

--- ftrot2/src/reducers/index.js
+++ ftrot2/src/reducers/index.js
@@ -3,10 +3,12 @@ import auth from "./AuthReducers/auth";
 import message from "./MessageReducers/message";
 import tasks from "./EngineerReducers/tasks";
 import workpackages from "./TechnicalProjectManagerReducers/workpackages";
+import leadEngineer from "./LeadEngineerReducers/leadEngineer";
 
 export default combineReducers({
   auth,
   message,
   tasks,
   workpackages,
+  leadEngineer,
 });
