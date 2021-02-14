2b3e457f76cfa68b28b9c6d5b29e21964ce1aa31
Mateusz Czerwinski <mateczerw@gmail.com>
2021-02-14 22:29:48 +0100

    splitted usefulinformation component

--- ftrot2/src/components/Engineer/Board/EngineerBoard.js
+++ ftrot2/src/components/Engineer/Board/EngineerBoard.js
@@ -2,9 +2,8 @@ import React from "react";
 import { Grid, Paper } from "@material-ui/core";
 import styled from "styled-components";
 import UserDetails from "./UserDetails";
-import UsefulInformationsLeft from "./UsefulInformations/UsefulInformationsLeft/UsefulInformationsLeft";
-import UsefulInformationRight from "./UsefulInformations/UsefulInformationsRight/UsefulInformationRight";
 import Diagrams from "./Diagrams/Diagrams";
+import UsefulInformations from "./UsefulInformations/UsefulInformations";
 
 const StyledBackground = styled.main`
   display: flex;
@@ -16,34 +15,18 @@ const StyledBackground = styled.main`
   font-size: 14px;
 `;
 
-const StyledUsefulInformations = styled(Paper)`
-  margin: 10px 0;
-  padding: 10px;
-  width: 100%;
-`;
-
 const EngineerBoard = () => {
   return (
     <StyledBackground>
       <Grid container spacing={3} justify="center">
-        {/* <section className="engineer board board__container"> */}
         <Grid item xs={8}>
           <UserDetails />
         </Grid>
         <Grid item container xs={8}>
-          <StyledUsefulInformations>
-            <Grid item container>
-              <Grid item xs={12} md={6}>
-                <UsefulInformationsLeft />
-              </Grid>
-              <Grid item container xs={12} md={6}>
-                <UsefulInformationRight />
-              </Grid>
-            </Grid>
-          </StyledUsefulInformations>
+          <UsefulInformations />
         </Grid>
         <Grid item xs={8}>
-          <Diagrams></Diagrams>
+          <Diagrams />
         </Grid>
       </Grid>
     </StyledBackground>
