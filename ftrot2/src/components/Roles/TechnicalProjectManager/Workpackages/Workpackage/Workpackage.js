01f7f538e291b24d3b2ff6574925ed184c048a43
Mateusz Czerwinski <mateczerw@gmail.com>
2021-03-10 00:25:55 +0100

    TJPM WorkPackage changed to SC

--- ftrot2/src/components/Roles/TechnicalProjectManager/Workpackages/Workpackage/Workpackage.js
+++ ftrot2/src/components/Roles/TechnicalProjectManager/Workpackages/Workpackage/Workpackage.js
@@ -1,11 +1,80 @@
 import React, { useState } from "react";
-import "./Workpackage.css";
 import WorkpackageEdit from "./WorkpackageEdit/WorkpackageEdit";
 import WorkpackageDetails from "./WorkpackageDetails/WorkpackageDetails";
 import Button from "@material-ui/core/Button";
 import EditIcon from "@material-ui/icons/Edit";
 import DeleteIcon from "@material-ui/icons/Delete";
 import InfoIcon from "@material-ui/icons/Info";
+import styled from "styled-components";
+
+const StyledWorkPackageContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin: 20px 0;
+  color: #efefef;
+`;
+
+const StyledWorkPackageDescription = styled.div`
+  background-color: #262729;
+  padding: 10px;
+  width: 60%;
+  height: 200px;
+  overflow-y: scroll;
+
+  & > h3 {
+    padding: 10px;
+  }
+  & > p {
+    width: 100%;
+  }
+
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`;
+
+const StyledWorkPackageDetails = styled.div`
+  background-color: #262729;
+  margin-right: 10px;
+  padding: 10px;
+  width: 25%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+`;
+
+const StyledWorkPackageActions = styled.div`
+  margin-left: 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
+  background-color: #262729;
+  width: 15%;
+
+  & > button {
+    margin: 5px;
+    width: 80%;
+    color: #93c5fd;
+    background-color: #00000033;
+    display: flex;
+    align-items: center;
+  }
+
+  & .MuiButton-label {
+    display: flex;
+    justify-content: flex-start;
+  }
+`;
+
+const StyledWorkPackageInfo = styled.p`
+  font-size: 15px;
+  & > b {
+    color: #b0b0b0;
+    font-weight: bolder;
+  }
+`;
+
 const Workpackage = ({
   id,
   name,
@@ -39,27 +108,27 @@ const Workpackage = ({
   };
 
   return (
-    <div className="tpjm workpackages workpackage">
-      <div className="tpjm workpackages workpackage__details">
+    <StyledWorkPackageContainer>
+      <StyledWorkPackageDetails>
         <h3>{name}</h3>
-        <p className="tpjm workpackages workpackage__info">
+        <StyledWorkPackageInfo>
           <b>Tasks quantity: </b> {tasksQuantity}
-        </p>
-        <p className="tpjm workpackages workpackage__info">
+        </StyledWorkPackageInfo>
+        <StyledWorkPackageInfo>
           <b>Finished tasks: </b> {finishedTasks}
-        </p>
-        <p className="tpjm workpackages workpackage__info">
+        </StyledWorkPackageInfo>
+        <StyledWorkPackageInfo>
           <b>Due to: </b> {deadline}
-        </p>
-        <p className="tpjm workpackages workpackage__info">
+        </StyledWorkPackageInfo>
+        <StyledWorkPackageInfo>
           <b>Predicted due to: </b> {predictedFinish}
-        </p>
-      </div>
-      <div className="tpjm workpackages workpackage__description">
+        </StyledWorkPackageInfo>
+      </StyledWorkPackageDetails>
+      <StyledWorkPackageDescription>
         <h3>Description:</h3>
         <p>{description}</p>
-      </div>
-      <div className="tpjm workpackages workpackage__actions">
+      </StyledWorkPackageDescription>
+      <StyledWorkPackageActions>
         <Button
           variant="contained"
           startIcon={<InfoIcon />}
@@ -92,8 +161,8 @@ const Workpackage = ({
         >
           Delete
         </Button>
-      </div>
-    </div>
+      </StyledWorkPackageActions>
+    </StyledWorkPackageContainer>
   );
 };
 
--- ftrot2/src/components/Roles/TechnicalProjectManager/Workpackages/Workpackages.js
+++ ftrot2/src/components/Roles/TechnicalProjectManager/Workpackages/Workpackages.js
@@ -1,23 +1,56 @@
 import { Button } from "@material-ui/core";
 import React, { useEffect, useState } from "react";
 import Workpackage from "./Workpackage/Workpackage";
-import "./Workpackages.css";
 import { getWorkPackages } from "../../../../actions/TechnicalProjectManagerActions/workpackagesActions";
-import moment from "moment";
 import WorkpackageAdd from "./WorkpackageAdd/WorkpackageAdd";
-import { uuid } from "uuidv4";
 import { useDispatch, useSelector } from "react-redux";
 import {
   createWorkpackage,
   deleteWorkpackage,
   editWorkpackage,
 } from "../../../../actions/TechnicalProjectManagerActions/workpackagesActions";
+import styled from "styled-components";
+
+const StyledWorkPackageBackground = styled.div`
+  display: flex;
+  background-color: #303133;
+  width: 100%;
+  min-height: calc(100vh - 58px);
+  justify-content: center;
+  color: #efefef;
+`;
+
+const StyledWorkPackageContainer = styled.div`
+  margin: 10px 0 0 0;
+  padding: 10px;
+  display: flex;
+  width: 80%;
+  flex-direction: column;
+`;
+
+const StyledWorkPackageHeader = styled.div`
+  background-color: #1d1d1f;
+  position: relative;
+  display: flex;
+  align-items: center;
+
+  & > button {
+    position: absolute;
+    left: 10px;
+  }
+`;
+
+const StyledWorkPackageTitle = styled.h2`
+  padding: 10px;
+  flex: 1;
+  display: flex;
+  justify-content: center;
+`;
 
 const Workpackages = () => {
-  const workpackagesFromSelector = useSelector(
+  const workpackages = useSelector(
     (state) => state.technicalProjectManager.workpackages
   );
-  const workpackages = JSON.parse(JSON.stringify(workpackagesFromSelector));
   const dispatch = useDispatch();
   const [isWorkPackageAddOpen, setIsWorkPackageAddOpen] = useState(false);
 
@@ -45,9 +78,9 @@ const Workpackages = () => {
   };
 
   return (
-    <div className="tpjm workpackages workpackages__background">
-      <div className="tpjm workpackages workpackages__container">
-        <div className="tpjm workpackages workpackages__header">
+    <StyledWorkPackageBackground>
+      <StyledWorkPackageContainer>
+        <StyledWorkPackageHeader>
           <Button
             variant="contained"
             color="secondary"
@@ -61,11 +94,9 @@ const Workpackages = () => {
             addWorkPackage={addWorkpackage}
           />
 
-          <h2 className="tpjm workpackages workpackages__title">
-            Workpackages
-          </h2>
-        </div>
-        <div className="tpjm workpackages workpackages__list">
+          <StyledWorkPackageTitle>Workpackages</StyledWorkPackageTitle>
+        </StyledWorkPackageHeader>
+        <div>
           {workpackages.map((workpackage) => (
             <Workpackage
               id={workpackage.id}
@@ -81,8 +112,8 @@ const Workpackages = () => {
             />
           ))}
         </div>
-      </div>
-    </div>
+      </StyledWorkPackageContainer>
+    </StyledWorkPackageBackground>
   );
 };
 
