d7b70483cd7992e0d0dc3059fb9ee1e59c6a6c67
Mateusz Czerwinski <mateczerw@gmail.com>
2021-03-18 22:58:59 +0100

    updated pictureupload with validation

--- ftrot2/src/components/Layout/Account/PictureUpload/PictureUpload.js
+++ ftrot2/src/components/Layout/Account/PictureUpload/PictureUpload.js
@@ -1,8 +1,8 @@
-import { Paper } from "@material-ui/core";
-import React, { useState } from "react";
+import { Button, Paper, TextField } from "@material-ui/core";
+import React, { useRef, useState } from "react";
 import styled from "styled-components";
 import * as yup from "yup";
-import { useFormik } from "formik";
+import { Formik, useFormik } from "formik";
 
 const StyledImage = styled.img`
   height: 500px;
@@ -15,43 +15,59 @@ const StyledPaper = styled(Paper)`
 `;
 
 //https://www.youtube.com/watch?v=tYGTjxhzrqY
-const validationSchema = yup.object({
-  picture: yup
+const validationSchema = yup.object().shape({
+  file: yup
     .mixed()
     .required("You need to provide a file")
-    .test("fileSize", "The file is too large", (value) => {
-      return value && value[0].size <= 2000000;
+    .test("size", "The file is too large", (value) => {
+      return value && value.size <= 2000000;
     })
     .test("type", "We only support jpeg", (value) => {
-      return value && value[0].type === "image/jpeg";
+      return value && value.type === "image/jpeg";
     }),
 });
 
 const PictureUpload = () => {
-  const formik = useFormik({
-    initialValues: {},
-    validationSchema: validationSchema,
-    onSubmit: (values) => {
-      handleImgInput(values);
-    },
-  });
-  const [picture, setPicture] = useState("");
-
-  const handleImgInput = (values) => {
-    setPicture(URL.createObjectURL(values.files[0]));
-  };
-
   return (
     <StyledPaper>
-      <input
-        type="file"
-        name="repeatedPassword"
-        value={formik.values.picture}
-        onChange={formik.handleChange.picture}
-        error={formik.touched.picture && Boolean(formik.errors.picture)}
-        helperText={formik.touched.picture && formik.errors.picture}
+      <Formik
+        initialValues={{ file: null }}
+        onSubmit={(values) => {
+          console.log(values);
+          // alert(
+          //   JSON.stringify(
+          //     {
+          //       fileName: values.file.name,
+          //       type: values.file.type,
+          //       size: `${values.file.size} bytes`,
+          //     },
+          //     null,
+          //     2
+          //   )
+          // );
+        }}
+        validationSchema={validationSchema}
+        render={({ values, handleSubmit, setFieldValue }) => {
+          return (
+            <form onSubmit={handleSubmit}>
+              <div className="form-group">
+                <input
+                  id="file"
+                  name="file"
+                  type="file"
+                  onChange={(event) => {
+                    setFieldValue("file", event.currentTarget.files[0]);
+                  }}
+                  className="form-control"
+                />
+              </div>
+              <button type="submit" className="btn btn-primary">
+                submit
+              </button>
+            </form>
+          );
+        }}
       />
-      {picture && <StyledImage src={picture} alt="Picture from input" />}
     </StyledPaper>
   );
 };
